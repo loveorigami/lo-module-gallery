51edf5ed12637309d6a518348a94c122b8536ec2
Loveorigami <loveorigami@mail.ru>
2016-08-24 11:02:21 +0300

    init repo

---
+++ models/Gallery.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace lo\modules\gallery\models;
+
+use Yii;
+
+
+/**
+ * This is the model class for table "gallery".
+ *
+ * @property integer $id
+ * @property string $slug
+ * @property string $title
+ * @property string $body
+ * @property string $view
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class Gallery extends \lo\core\db\ActiveRecord
+{
+    const STATUS_DRAFT = 0;
+    const STATUS_PUBLISHED = 1;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%gallery}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function metaClass()
+    {
+        return GalleryMeta::class;
+    }
+
+}
