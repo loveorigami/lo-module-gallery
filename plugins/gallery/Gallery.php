fb71db032f61a96505371443ba07eefe7aa5b2c0
Loveorigami <loveorigami@mail.ru>
2016-12-16 13:56:31 +0300

    gallery shortcode

---
+++ plugins/gallery/Gallery.php
@@ -0,0 +1,46 @@
+<?php
+namespace lo\modules\gallery\plugins\gallery;
+
+use lo\plugins\components\BasePlugin;
+use yii\web\View;
+
+/**
+ * Plugin Name: Gallery
+ * Version: 1.0
+ * Plugin URI:
+ * Description: A simple gallery plugin for use shortcode [gallery id=1]
+ * Author: Andrey Lukyanov
+ * Author URI: https://github.com/loveorigami/yii2-plugins-system
+ */
+class Gallery extends BasePlugin
+{
+    /**
+     * @return array
+     */
+    public static function events()
+    {
+        return [
+            View::class => [
+                View::EVENT_AFTER_RENDER => ['shortcode']
+            ]
+        ];
+    }
+
+    /**
+     * @param $event
+     * @return bool
+     */
+    public static function shortcode($event)
+    {
+        if (isset($event->output)) {
+
+            $shortcode = self::getShortcode([
+                'gallery' => ['lo\modules\gallery\widgets\GalleryShortcode', 'widget']
+            ]);
+
+            $event->output = $shortcode->parse($event->output);
+        }
+
+        return null;
+    }
+}
---
+++ widgets/GalleryShortcode.php
@@ -0,0 +1,117 @@
+<?php
+namespace lo\modules\gallery\widgets;
+
+use lo\modules\gallery\models\GalleryCat;
+use lo\modules\gallery\repository\ImageRepository;
+use lo\plugins\components\ShortcodeWidget;
+use yii\data\ActiveDataProvider;
+
+/**
+ * Class GalleryWidget
+ * @package lo\modules\gallery\plugins\gallery
+ * @author Lukyanov Andrey <loveorigami@mail.ru>
+ */
+class GalleryShortcode extends ShortcodeWidget
+{
+    /**
+     * Default view
+     * @var string
+     */
+    const VIEW = 'gallery-show';
+
+    /**
+     * Default columns
+     * @var int
+     */
+    const COLS = 6;
+
+    /**
+     * Default columns
+     * @var int
+     */
+    const LIMIT = 60;
+
+    /**
+     * Rendered view
+     * @var string
+     */
+    public $view;
+
+    /**
+     * Columns in row
+     * @var int
+     */
+    public $cols;
+
+    /**
+     * Limit images on page
+     * @var int
+     */
+    public $limit;
+
+    /**
+     * @var integer
+     */
+    public $id;
+
+    /**
+     * Init widget
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (!$this->view) {
+            $this->view = self::VIEW;
+        }
+
+        if (!$this->cols) {
+            $this->cols = self::COLS;
+        }
+
+        if (!$this->limit) {
+            $this->limit = self::LIMIT;
+        }
+    }
+
+    /**
+     * Render widget
+     * @return string
+     */
+    public function run()
+    {
+        if ($this->id) {
+            /** @var GalleryCat $model */
+            $model = GalleryCat::find()->where(['id' => $this->id])->limit(1)->published()->one();
+
+            if ($model) {
+                /** @var ImageRepository $gallery */
+                $gallery = $model->getBehavior($model::GALLERY_ONE);
+
+                $query = $gallery->findImages()->published();
+                $query->limit($this->limit);
+
+                $dataProvider = new ActiveDataProvider([
+                    'query' => $query,
+                    'pagination' => false
+                ]);
+
+                $viewParams = [
+                    'gallery' => $gallery,
+                    'thumb' => $model::THUMB_TMB,
+                    'big' => $model::THUMB_BIG,
+                    'cols' => $this->cols
+                ];
+
+                return $this->render($this->view, [
+                    'dataProvider' => $dataProvider,
+                    'viewParams' => $viewParams,
+                    'id' => 'w'.$this->id
+                ]);
+
+            }
+            return null;
+        }
+        return null;
+    }
+}
